4df0da64005fab8d710ed055d1a996be7e2942fd
stefaan <stefaan@gim.be>
2014-12-08 17:20:11 +0100

    ACCESS CONTROL ALLOW HEADERS.

--- eenvplus-sdi-services/src/main/java/be/vmm/eenvplus/sdi/plugins/providers/cors/CorsInterceptor.java
+++ eenvplus-sdi-services/src/main/java/be/vmm/eenvplus/sdi/plugins/providers/cors/CorsInterceptor.java
@@ -58,6 +58,7 @@ public class CorsInterceptor implements MessageBodyWriterInterceptor {
 		context.getHeaders().add(ACCESS_CONTROL_ALLOW_ORIGIN, "*");
 		context.getHeaders().add(ACCESS_CONTROL_ALLOW_METHODS,
 				"GET, POST, PUT, DELETE, OPTIONS, HEAD");
+		context.getHeaders().add(ACCESS_CONTROL_ALLOW_HEADERS, "*");
 
 		context.proceed();
 	}
